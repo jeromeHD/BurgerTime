a1ecf8c622f89279b6a9aaee3afadd9f28e0afd4
JeromeHD <jaymagolan@gmail.com>
2019-04-07 13:33:13 -0400

    db handlebars server

---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Shrimp and Grits Burger');
+INSERT INTO burgers (burger_name) VALUES ('7 Cheese Stop Up Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hungover Burger');
